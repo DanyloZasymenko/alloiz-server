b344915bd5574a2db9c5ebd162e5d1da5d023211
DanyloZasymenko <danik1492@gmail.com>
2018-06-26 15:57:52 +0300

    first commit

---
+++ src/main/java/com/alloiz/alloizserver/AlloizServerApplication.java
@@ -0,0 +1,12 @@
+package com.alloiz.alloizserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlloizServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlloizServerApplication.class, args);
+    }
+}
