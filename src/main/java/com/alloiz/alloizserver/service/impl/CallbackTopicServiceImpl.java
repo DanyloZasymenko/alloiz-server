8ccee00873ac1cfc08ded81f2016e6c3c45acc57
Bohdan Rud <bohdanrud5180@gmail.com>
2018-06-27 17:08:09 +0300

    added CallbackTopicServiceImpl

--- src/main/java/com/alloiz/alloizserver/service/impl/CallbackTopicServiceImpl.java
+++ src/main/java/com/alloiz/alloizserver/service/impl/CallbackTopicServiceImpl.java
@@ -1,6 +1,57 @@
 package com.alloiz.alloizserver.service.impl;
 
+import com.alloiz.alloizserver.model.CallbackTopic;
+import com.alloiz.alloizserver.repository.CallbackTopicRepository;
 import com.alloiz.alloizserver.service.CallbackTopicService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CallbackTopicServiceImpl implements CallbackTopicService {
+
+  @Autowired
+  private CallbackTopicRepository callbackTopicRepository;
+
+  @Override
+  public CallbackTopic findOneAvailable(Long id) {
+    return callbackTopicRepository.findByAvailableAndId(true,id);
+  }
+
+  @Override
+  public List<CallbackTopic> findAllAvailable() {
+    return callbackTopicRepository.findAllByAvailable(true);
+  }
+
+  @Override
+  public CallbackTopic findOne(Long id) {
+    return callbackTopicRepository.getOne(id);
+  }
+
+  @Override
+  public List<CallbackTopic> findAll() {
+    return callbackTopicRepository.findAll();
+  }
+
+  @Override
+  public void save(CallbackTopic callbackTopic) {
+    callbackTopicRepository.save(callbackTopic);
+  }
+
+  @Override
+  public Boolean deleteById(Long id) {
+    if(id != null && id >= 0){
+      CallbackTopic callbackTopic = callbackTopicRepository.findOne(id);
+      if(callbackTopic != null){
+        callbackTopicRepository.delete(callbackTopic);
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    else{
+      throw new NullPointerException("Id is null or less than zero");
+    }
+  }
 }
