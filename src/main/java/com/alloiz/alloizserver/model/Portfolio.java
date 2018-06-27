f9b27a541ad42d283d933957596494252a169063
Bohdan Rud <bohdanrud5180@gmail.com>
2018-06-27 11:52:25 +0300

    added Portfolio

--- src/main/java/com/alloiz/alloizserver/model/Portfolio.java
+++ src/main/java/com/alloiz/alloizserver/model/Portfolio.java
@@ -1,4 +1,55 @@
 package com.alloiz.alloizserver.model;
 
-public class Portfolio {
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+
+public class Portfolio extends General{
+
+  private String link;
+
+  private List<String> images;
+
+  @Column(columnDefinition = "LONGTEXT")
+  private String description;
+
+  public Portfolio() {
+    images = new ArrayList<String>();
+  }
+
+  public String getLink() {
+    return link;
+  }
+
+  public void setLink(String link) {
+    this.link = link;
+  }
+
+  public List<String> getImages() {
+    return images;
+  }
+
+  public void setImages(List<String> images) {
+    this.images = images;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  @Override
+  public String toString() {
+    return "Portfolio{" +
+        "link='" + link + '\'' +
+        ", images=" + images +
+        ", description='" + description + '\'' +
+        ", id=" + id +
+        ", name='" + name + '\'' +
+        ", available=" + available +
+        "} " + super.toString();
+  }
 }
