5dce3c67b09220536fe99bc50ab64b2feb4b010b
DanyloZasymenko <danik1492@gmail.com>
2018-06-27 14:08:54 +0300

    added service for Technologies,CallBack and Worker

--- src/main/java/com/alloiz/alloizserver/service/TechnologiesService.java
+++ src/main/java/com/alloiz/alloizserver/service/TechnologiesService.java
@@ -1,4 +1,22 @@
 package com.alloiz.alloizserver.service;
 
+import com.alloiz.alloizserver.model.Technologies;
+
+import java.util.List;
+
 public interface TechnologiesService {
+
+    Technologies findOneAvaliable(Long id);
+
+    List<Technologies> findAllAvaliable();
+
+    Technologies findOne(Long id);
+
+    List<Technologies> findAll();
+
+    void save(Technologies technologies);
+
+    Boolean delete(Long id);
 }
+
+
