e4d1562bda365801c1115650474350e95b4c5181
xadriy <xandriy58@gmail.com>
2018-06-27 16:53:27 +0300

    addded WorkerServimpl

--- src/main/java/com/alloiz/alloizserver/service/impl/WorkerServiceImpl.java
+++ src/main/java/com/alloiz/alloizserver/service/impl/WorkerServiceImpl.java
@@ -1,4 +1,58 @@
 package com.alloiz.alloizserver.service.impl;
 
-public class WorkerServiceImpl {
+import com.alloiz.alloizserver.model.Worker;
+import com.alloiz.alloizserver.repository.WorkerRepository;
+import com.alloiz.alloizserver.service.WorkerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WorkerServiceImpl implements WorkerService {
+
+    @Autowired
+    private WorkerRepository workerRepository;
+
+    @Override
+    public Worker findOneAvailable(Long id) {
+        return workerRepository.findByAvailableAndId(true, id);
+    }
+
+    @Override
+    public List<Worker> findAllAvailable() {
+        return workerRepository.findAllByAvailable(true);
+    }
+
+    @Override
+    public Worker findOne(Long id) {
+        return workerRepository.findOne(id);
+    }
+
+    @Override
+    public List<Worker> findAll() {
+        return workerRepository.findAll();
+    }
+
+    @Override
+    public void save(Worker worker) {
+        workerRepository.save(worker);
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        if(id != null && id >= 0){
+            Worker worker = workerRepository.findOne(id);
+            if(worker != null){
+                workerRepository.delete(worker);
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else{
+            throw new NullPointerException("Id is null or less than zero");
+        }
+    }
 }
