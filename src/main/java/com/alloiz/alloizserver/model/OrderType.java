050fe15221afd08f09b9f72217d47eca7dcc6c32
xadriy <xandriy58@gmail.com>
2018-06-27 12:49:06 +0300

    added models OrderType and Worker

--- src/main/java/com/alloiz/alloizserver/model/OrderType.java
+++ src/main/java/com/alloiz/alloizserver/model/OrderType.java
@@ -1,4 +1,16 @@
 package com.alloiz.alloizserver.model;
 
-public class OrderType {
+public class OrderType extends General{
+
+    public OrderType() {
+    }
+
+    @Override
+    public String toString() {
+        return "OrderType{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", available=" + available +
+                '}';
+    }
 }
--- src/main/java/com/alloiz/alloizserver/model/Worker.java
+++ src/main/java/com/alloiz/alloizserver/model/Worker.java
@@ -1,4 +1,51 @@
 package com.alloiz.alloizserver.model;
 
-public class Worker {
+public class Worker extends General{
+    private String name;
+    private String surname;
+    private String image;
+
+    public Worker() {
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Worker setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Worker setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Worker setImage(String image) {
+        this.image = image;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", image='" + image + '\'' +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", available=" + available +
+                '}';
+    }
 }
