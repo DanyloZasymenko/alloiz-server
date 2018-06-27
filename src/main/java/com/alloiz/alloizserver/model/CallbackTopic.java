362c378fa91478f3eaefb9b2ce87b1255416efcc
xadriy <xandriy58@gmail.com>
2018-06-27 18:22:56 +0300

    added CallBackDto

--- src/main/java/com/alloiz/alloizserver/model/CallbackTopic.java
+++ src/main/java/com/alloiz/alloizserver/model/CallbackTopic.java
@@ -5,20 +5,20 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
 @Entity
-public class CallbackTopic extends General{
+public class CallbackTopic extends General {
 
-  @ManyToOne(cascade = CascadeType.REFRESH)
-  private Callback callback;
+    @ManyToOne(cascade = CascadeType.REFRESH)
+    private Callback callback;
 
-  public CallbackTopic() {
-  }
+    public CallbackTopic() {
+    }
 
-  @Override
-  public String toString() {
-    return "CallbackTopic{" +
-        "id=" + id +
-        ", name='" + name + '\'' +
-        ", available=" + available +
-        "} " + super.toString();
-  }
+    @Override
+    public String toString() {
+        return "CallbackTopic{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", available=" + available +
+                "} " + super.toString();
+    }
 }
