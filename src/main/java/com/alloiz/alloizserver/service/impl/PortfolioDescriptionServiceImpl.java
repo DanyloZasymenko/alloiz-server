548ba323ff04aefe8797d39c2ee40a362956205b
Bohdan Rud <bohdanrud5180@gmail.com>
2018-06-28 14:34:48 +0300

    added PD service impl

--- src/main/java/com/alloiz/alloizserver/service/impl/PortfolioDescriptionServiceImpl.java
+++ src/main/java/com/alloiz/alloizserver/service/impl/PortfolioDescriptionServiceImpl.java
@@ -1,45 +1,67 @@
 package com.alloiz.alloizserver.service.impl;
 
 import com.alloiz.alloizserver.model.PortfolioDescription;
+import com.alloiz.alloizserver.repository.PortfolioDescriptionRepository;
 import com.alloiz.alloizserver.service.PortfolioDescriptionService;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PortfolioDescriptionServiceImpl implements PortfolioDescriptionService {
 
+  @Autowired
+  private PortfolioDescriptionRepository portfolioDescriptionRepository;
+
   @Override
   public PortfolioDescription findOneAvailable(Long id) {
-    return null;
+    return portfolioDescriptionRepository.findByAvailableAndId(true, id);
   }
 
   @Override
   public List<PortfolioDescription> findAllAvailable() {
-    return null;
+    return portfolioDescriptionRepository.findAllByAvailable(true);
   }
 
   @Override
   public PortfolioDescription findOne(Long id) {
-    return null;
+    return portfolioDescriptionRepository.findOne(id);
   }
 
   @Override
   public List<PortfolioDescription> findAll() {
-    return null;
+    return portfolioDescriptionRepository.findAll();
   }
 
   @Override
-  public PortfolioDescription save(PortfolioDescription portfolioDescription) {
-    return null;
-  }
-
-  @Override
-  public Boolean delete(Long id) {
-    return null;
+  public PortfolioDescription save(PortfolioDescription portfolio) {
+    return portfolioDescriptionRepository.save(portfolio);
   }
 
   @Override
   public PortfolioDescription update(PortfolioDescription portfolioDescription) {
-    return null;
+    return save(findOne(portfolioDescription.getId())
+        .setDescription(portfolioDescription.getDescription())
+        .setLanguage(portfolioDescription.getLanguage())
+        .setAvailable(portfolioDescription.getAvailable())
+    );
+
   }
+
+  @Override
+  public Boolean delete(Long id) {
+    if (id != null && id >= 0) {
+      PortfolioDescription portfolioDescription = portfolioDescriptionRepository.findOne(id);
+      if (portfolioDescription != null) {
+        portfolioDescriptionRepository.delete(portfolioDescription);
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      throw new NullPointerException("Id is null or less than zero");
+    }
+  }
+
+
 }
