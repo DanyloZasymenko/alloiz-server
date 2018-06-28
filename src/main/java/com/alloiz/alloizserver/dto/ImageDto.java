d2b7b16b39f92b4f4d05918073184e369b410a1f
Bohdan Rud <bohdanrud5180@gmail.com>
2018-06-28 14:50:29 +0300

    addded DTO

--- src/main/java/com/alloiz/alloizserver/dto/ImageDto.java
+++ src/main/java/com/alloiz/alloizserver/dto/ImageDto.java
@@ -1,4 +1,58 @@
 package com.alloiz.alloizserver.dto;
 
+import com.alloiz.alloizserver.dto.utils.annotations.Dto;
+
+@Dto
 public class ImageDto {
+
+  protected String path;
+  protected Long id;
+  protected String name;
+  protected Boolean available;
+
+  public String getPath() {
+    return path;
+  }
+
+  public ImageDto setPath(String path) {
+    this.path = path;
+    return this;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public ImageDto setId(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public ImageDto setName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public Boolean getAvailable() {
+    return available;
+  }
+
+  public ImageDto setAvailable(Boolean available) {
+    this.available = available;
+    return this;
+  }
+
+  @Override
+  public String toString() {
+    return "ImageDto{" +
+        "path='" + path + '\'' +
+        ", id=" + id +
+        ", name='" + name + '\'' +
+        ", available=" + available +
+        '}';
+  }
 }
--- src/main/java/com/alloiz/alloizserver/dto/TechnologyDescriptionDto.java
+++ src/main/java/com/alloiz/alloizserver/dto/TechnologyDescriptionDto.java
@@ -1,4 +1,60 @@
 package com.alloiz.alloizserver.dto;
 
+import com.alloiz.alloizserver.dto.utils.annotations.Dto;
+import com.alloiz.alloizserver.model.Technology;
+import com.alloiz.alloizserver.model.enums.Language;
+
+@Dto
 public class TechnologyDescriptionDto {
+
+  private Technology technology;
+  private Language language;
+  protected Long id;
+  protected Boolean available;
+
+  public Technology getTechnology() {
+    return technology;
+  }
+
+  public TechnologyDescriptionDto setTechnology(Technology technology) {
+    this.technology = technology;
+    return this;
+  }
+
+  public Language getLanguage() {
+    return language;
+  }
+
+  public TechnologyDescriptionDto setLanguage(Language language) {
+    this.language = language;
+    return this;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public TechnologyDescriptionDto setId(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  public Boolean getAvailable() {
+    return available;
+  }
+
+  public TechnologyDescriptionDto setAvailable(Boolean available) {
+    this.available = available;
+    return this;
+  }
+
+  @Override
+  public String toString() {
+    return "TechnologyDescriptionDto{" +
+        "technology=" + technology +
+        ", language=" + language +
+        ", id=" + id +
+        ", available=" + available +
+        '}';
+  }
 }
