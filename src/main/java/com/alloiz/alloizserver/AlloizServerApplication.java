f659d7167b85ffb234c883e0ced1b86c6703b18d
Nazar Sekh <Nazar.sekh@gmail.com>
2018-06-28 15:36:43 +0300

    add @EnableJpaRepositories

--- src/main/java/com/alloiz/alloizserver/AlloizServerApplication.java
+++ src/main/java/com/alloiz/alloizserver/AlloizServerApplication.java
@@ -2,8 +2,10 @@ package com.alloiz.alloizserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.alloiz.alloizserver.repository.")
 public class AlloizServerApplication {
 
     public static void main(String[] args) {
