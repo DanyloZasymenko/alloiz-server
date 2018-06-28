217610e66c916ec9a29b05508ff0388b7f453626
Bohdan Rud <bohdanrud5180@gmail.com>
2018-06-28 11:14:03 +0300

    added WorkerFullDto

--- src/main/java/com/alloiz/alloizserver/dto/WorkerShortDto.java
+++ src/main/java/com/alloiz/alloizserver/dto/WorkerShortDto.java
@@ -3,7 +3,7 @@ package com.alloiz.alloizserver.dto;
 import com.alloiz.alloizserver.dto.utils.annotations.Dto;
 
 @Dto
-public class WorkerShortDto {
+public class WorkerShortDto <T extends WorkerShortDto>{
 
     protected Long id;
     protected String name;
@@ -11,57 +11,50 @@ public class WorkerShortDto {
     protected String image;
     protected Boolean available;
 
-    public WorkerShortDto(Long id, String name, String surname, String image, Boolean available) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.image = image;
-        this.available = available;
-    }
-
-    public Boolean getAvailable() {
-        return available;
-    }
-
-    public WorkerShortDto setAvailable(Boolean available) {
-        this.available = available;
-        return this;
-    }
 
     public Long getId() {
         return id;
     }
 
-    public WorkerShortDto setId(Long id) {
+    public T setId(Long id) {
         this.id = id;
-        return this;
+        return (T) this;
     }
 
     public String getName() {
         return name;
     }
 
-    public WorkerShortDto setName(String name) {
+    public T setName(String name) {
         this.name = name;
-        return this;
+        return (T) this;
     }
 
     public String getSurname() {
         return surname;
     }
 
-    public WorkerShortDto setSurname(String surname) {
+    public T setSurname(String surname) {
         this.surname = surname;
-        return this;
+        return (T) this;
     }
 
     public String getImage() {
         return image;
     }
 
-    public WorkerShortDto setImage(String image) {
+    public T setImage(String image) {
         this.image = image;
-        return this;
+        return (T) this;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public T setAvailable(Boolean available) {
+        this.available = available;
+        return (T) this;
     }
 
     @Override
